7f9e81236afd8d3b2ccde4d72186d9e18996be4d
Gastón Garcia Juri <gastongarciajuri@gmail.com>
2024-09-11 17:36:26 -0300

    progress

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -19,16 +19,7 @@ const LandingPage = () => {
           Ver Progreso
           </Link>
         </button>
-        <button className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
-          <Link to={'/course'}>
-          Ver Progreso
-          </Link>
-        </button>
-        <button className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
-          <Link to={'/course'}>
-          Ver Progreso
-          </Link>
-        </button>
+
       </div>
     </Layout>
   );
